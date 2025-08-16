f77153594815bebba82799f3745391f0a1173af7
MarcMaier-QA <marc_maier1990@t-online.de>
2025-08-16 17:10:19 +0200

    cleanup main: remove unused WORDS list and keep only play_game import

--- snowman.py
+++ snowman.py
@@ -1,7 +1,5 @@
-from game_logic import get_random_word, display_game_state, play_game
+from game_logic import play_game
 
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
 if __name__ == "__main__":
     play_game()
