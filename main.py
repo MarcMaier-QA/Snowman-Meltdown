756188cd50c82bc58a24bf07a85da127b3b70372
MarcMaier-QA <marc_maier1990@t-online.de>
2025-08-18 12:33:45 +0200

    Refactor: main loop: move game loop into main() function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from game_logic import play_game
 
 
-if __name__ == "__main__":
+def main():
+    """
+    Start the game and handle replay logic.
+    """
     while True:
         play_game()
 
@@ -9,9 +12,12 @@ if __name__ == "__main__":
         while True:
             again = input("Do you want to play again? (y/n): ").lower()
             if again in ["y", "n"]:
-                break # valid input, exit loop
-            print("Please enter 'y' for yes or 'n' fo no.")
+                break  # valid input, exit loop
+            print("Please enter 'y' for yes or 'n' for no.")
 
         if again == "n":
             print("Thanks for playing! Goodbye!")
             break
+
+if __name__ == "__main__":
+    main()
